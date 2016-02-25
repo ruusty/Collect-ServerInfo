83a294058010a394fdd58adb2aaa17e1dadde54d
ruusty <russell@holliday.id.au>
2016-02-25 13:12:20 +1100

    Added template file

---
+++ Template.Section.Collect-ServerInfo.ps1
@@ -0,0 +1,38 @@
+﻿<#
+Template for adding to Collect-ServerInfo
+#>
+
+#---------------------------------------------------------------------
+# Collect computer system information and convert to HTML fragment
+#---------------------------------------------------------------------
+
+Write-Verbose "Collecting computer system information"
+
+$subhead = "<h3>Computer System Information</h3>"
+$htmlbody += $subhead
+
+try
+{
+  $csinfo = Get-WmiObject Win32_ComputerSystem -ComputerName $ComputerName -ErrorAction STOP |
+  Select-Object Name, Manufacturer, Model,
+                @{ Name = 'Physical Processors'; Expression = { $_.NumberOfProcessors } },
+                @{ Name = 'Logical Processors'; Expression = { $_.NumberOfLogicalProcessors } },
+                @{
+    Name = 'Total Physical Memory (Gb)'; Expression = {
+      $tpm = $_.TotalPhysicalMemory/1GB;
+      "{0:F0}" -f $tpm
+    }
+  },
+                DnsHostName, Domain
+  
+  $htmlbody += $csinfo | ConvertTo-Html -Fragment
+  $htmlbody += $spacer
+  
+}
+catch
+{
+  Write-Warning $_.Exception.Message
+  $htmlbody += "<p>An error was encountered. $($_.Exception.Message)</p>"
+  $htmlbody += $spacer
+}
+
